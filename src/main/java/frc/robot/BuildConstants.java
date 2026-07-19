aa5fb8726271078023bdc7c82798119abd8ca48f
CorianderSeed <github.unscrew076@slmails.com>
2026-07-18 20:46:25 -0500

    I'm not sure what I'm commiting

---
+++ src/main/java/frc/robot/BuildConstants.java
@@ -0,0 +1,17 @@
+package frc.robot;
+
+/** Automatically generated file containing build version information. */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "PrototypERRsummer";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 3;
+  public static final String GIT_SHA = "7df6dbd9a00d2b0fa154298d34bf16a12da39161";
+  public static final String GIT_DATE = "2026-07-15 21:57:14 EDT";
+  public static final String GIT_BRANCH = "main";
+  public static final String BUILD_DATE = "2026-07-16 09:29:08 EDT";
+  public static final long BUILD_UNIX_TIME = 1784208548326L;
+  public static final int DIRTY = 0;
+
+  private BuildConstants() {}
+}
